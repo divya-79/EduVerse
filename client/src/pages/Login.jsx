cd1d69619b751b5a02ac15224ae4abaa5a1fee97
divya-79 <goeldivyanshi99@gmail.com>
2026-07-11 20:30:00 +0530

    Add authentication UI for login and register pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,45 @@
+import { Link } from "react-router-dom";
+import "./Login.css";
+
 function Login() {
   return (
-    <div>
-      <h1>🔐 Login Page</h1>
+    <div className="login-container">
+
+      <div className="login-card">
+
+        <h2>Welcome Back</h2>
+
+        <p>Login to your EduVerse Account</p>
+
+        <form>
+
+          <label htmlFor="email">Email</label>
+
+          <input
+            id="email"
+            type="email"
+            placeholder="Enter your email"
+          />
+
+          <label htmlFor="password">Password</label>
+
+          <input
+            id="password"
+            type="password"
+            placeholder="Enter your password"
+          />
+
+          <button>Login</button>
+
+        </form>
+
+        <p>
+          Don't have an account?{" "}
+          <Link to="/register">Register</Link>
+        </p>
+
+      </div>
+
     </div>
   );
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,57 @@
+import { Link } from "react-router-dom";
+import "./Login.css";
+
 function Register() {
   return (
-    <div>
-      <h1>📝 Register Page</h1>
+    <div className="login-container">
+
+      <div className="login-card">
+
+        <h2>Create Account</h2>
+
+        <p>Join EduVerse and start learning today.</p>
+
+        <form>
+          <label htmlFor="name">Name</label>
+          <input 
+          id="name"
+          type="text"
+          placeholder="Enter your name" />
+
+          <label htmlFor="email">Email</label>
+
+          <input
+            id="email"
+            type="email"
+            placeholder="Enter your email"
+          />
+
+          <label htmlFor="password">Password</label>
+
+          <input
+            id="password"
+            type="password"
+            placeholder="Enter your password"
+          />
+          <label htmlFor="password">Confirm Password</label>
+
+          <input
+            id="password"
+            type="password"
+            placeholder="Confirm Your Password"
+          />
+
+          <button>Register</button>
+
+        </form>
+
+        <p>
+          Already have an account?{" "}
+          <Link to="/login">Login</Link>
+        </p>
+
+      </div>
+
     </div>
   );
 }
